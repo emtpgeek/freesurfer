b00957f5fd4298a161300b7c82361ee1631c6038
Bevin R Brett <bevin_brett@hotmail.com>
2018-09-14 19:10:34 +1200

    test_TriangleFile_readWrite PASS

--- utils/test/test_TriangleFile_readWrite.c
+++ utils/test/test_TriangleFile_readWrite.c
@@ -1,36 +1,46 @@
 #include "stdio.h"
 
 #include "mrisurf.h"
+#include "../mrisurf_topology.h"
 
 int main() {
 
-  int const max_vertices = 4, max_faces = 2, 
-               nvertices = 4,    nfaces = 2;
-
-  MRIS mrisObject;
-  MRIS* src = &mrisObject;
+  int fails = 0;
   
-  MRISctr(src, max_vertices, max_faces, nvertices, nfaces);
-  CHECK(src->nvertices == 4);
-  CHECK(src->nfaces == 2);
-   
-  mrisAddEdge(src, 0, 1);                   // will get removed
+  int const max_vertices = 4, max_faces = 2, 
+               nvertices = 4,    nfaces = 0;
+
+  MRIS* src = MRISoverAlloc(max_vertices, max_faces, nvertices, nfaces);
+  
+  mrisAddEdge(src, 0, 1);
   mrisAddEdge(src, 0, 2);
   mrisAddEdge(src, 0, 3);
   mrisAddEdge(src, 1, 2);
   mrisAddEdge(src, 2, 3);
   mrisAddEdge(src, 3, 1);
 
-  mrisAttachFaceToVertices(src, 0, 0,1,2);  // will get removed
-  mrisAttachFaceToVertices(src, 1, 1,2,3);  // will survive
+  mrisAddFace(src, 0,1,2);
+  mrisAddFace(src, 1,2,3);
 
   const char* fnm = "./test_TriangleFile_readWrite.tmp";
   printf("MRISwrite %s returned %d\n",
-    MRISwrite(src, fnm));
+    fnm, MRISwrite(src, fnm));
     
   MRIS* dst = MRISread(fnm) ;
   printf("MRISread %s returned mris with dims nvertices:%d nfaces:%d\n",
-    dst->nvertices, dst->nfaces);
+    fnm, dst->nvertices, dst->nfaces);
 
-  return 0;
+  if (src->nvertices != dst->nvertices) {
+    fails++;
+    printf("FAIL src->nvertices:%d != dst->nvertices:%d\n",
+      dst->nvertices, dst->nfaces);
+  }
+  
+  if (src->nfaces != dst->nfaces) {
+    fails++;
+    printf("FAIL src->nfaces:%d != dst->nfaces:%d\n",
+      dst->nfaces, dst->nfaces);
+  }
+  
+  return fails ? 1 : 0;
 }
